c6b95b5c2c94aa8c689e088990c06eeab35cea7c
gabriel <gabrieldecarvalhu@gmail.com>
2024-10-18 10:20:43 -0300

    desenvolvendo paginacao

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -101,11 +101,32 @@ export default function DashBoard() {
 
                         </tbody>
                     </table>
+                    <nav className="flex items-center gap-x-1" aria-label="Pagination">
+                        <button type="button" className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-200 dark:focus:bg-white/10" aria-label="Previous">
+                            <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <path d="m15 18-6-6 6-6"></path>
+                            </svg>
+                            <span className="sr-only">Previous</span>
+                        </button>
+                        <div className="flex items-center gap-x-1">
+                            <span className="min-h-[38px] min-w-[38px] flex justify-center items-center border border-gray-100 text-gray-800 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-300 dark:text-black dark:focus:bg-black/10">1</span>
+                            <span className="min-h-[38px] flex justify-center items-center text-gray-500 py-2 px-1.5 text-sm dark:text-neutral-500">of</span>
+                            <span className="min-h-[38px] flex justify-center items-center text-gray-500 py-2 px-1.5 text-sm dark:text-neutral-500">3</span>
+                        </div>
+                        <button type="button" className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-gray-800 hover:bg-gray-800 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-200 dark:focus:bg-white/10" aria-label="Next">
+                            <span className="sr-only">Next</span>
+                            <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <path d="m9 18 6-6-6-6"></path>
+                            </svg>
+                        </button>
+                    </nav>
 
                 </div>
 
 
             </div>
+
+
 
 
         </section>
